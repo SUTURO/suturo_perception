13a7340851adff77a61d516020031c9fe9ce58d3
Evan Kapitzke <evan@uni-bremen.de>
2019-11-17 21:27:05 +0100

    RS Package with configuration files for HSR

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # suturo_perception
+Start Pipeline with:
+rosrun robosherlock run _ae:=hsr_demo _vis:=true
+
+Set _vis to false if no visual feedback is needed.
+
---
+++ rs_perception/cmake/rs_perception_config.cmake
@@ -0,0 +1,6 @@
+set(rs_perception_NAMESPACE           rs_perception)
+set(rs_perception_TYPESYSTEM_CPP_PATH /home/ros/catkin_ws/src/rs_perception/include/rs_perception/types)
+set(rs_perception_TYPESYSTEM_XML_PATH /home/ros/catkin_ws/src/rs_perception/descriptors/typesystem)
+set(rs_perception_ANNOTATOR_PATH      /home/ros/catkin_ws/src/rs_perception/descriptors/annotators)
+set(rs_perception_ENGINE_PATH         /home/ros/catkin_ws/src/rs_perception/descriptors/analysis_engines)
+
