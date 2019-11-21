1615b65dc96908777aae99847cce216fafd53077
Skadir <janschimpf@web.de>
2019-11-21 22:27:28 +0100

    leichte anpassungen and die sub.py und habe die manipulation.py mit in den script orderner geschoben

--- rs_perception/script/sub.py
+++ rs_perception/script/sub.py
@@ -3,6 +3,7 @@ import rospy
 import json
 from robosherlock_msgs.msg import RSObjectDescriptions
 
+
 def callback(data):
     value = data.obj_descriptions[0]
     vector = value[value.find("\"translation\":")+15:value.find("\"frame\"")-2].split(",", 3)
@@ -10,8 +11,8 @@ def callback(data):
     x = float(vector[0])
     y = float(vector[1])
     z = float(vector[2])
-
-    rospy.loginfo("Got x = " + str(x) + ", y = " + str(y) + ", z = " + str(z) + "\n")
+    
+    rospy.loginfo(str(x) + " " + str(y) + " " + str(z) + "\n")
     
 def listener():
 
