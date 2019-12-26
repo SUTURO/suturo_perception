843a997869f8c39be39ad385f74e839588cb5a6c
JeremiasThun <thun@uni-bremen.de>
2019-12-27 00:38:37 +0100

    trigger via actionserver implemented

--- actionserver/src/perception_server.cpp
+++ actionserver/src/perception_server.cpp
@@ -2,6 +2,10 @@
 #include <actionlib/server/simple_action_server.h>
 #include <suturo_perception_msgs/ExtractObjectInfoAction.h>
 
+#include <cstdlib>
+#include <std_srvs/Trigger.h>
+#include <std_msgs/String.h>
+
 class PerceptionServer
 {
 protected:
@@ -32,32 +36,37 @@ public:
         ros::Rate r(1);
         bool success = true;
 
-        // push_back the seeds for the fibonacci sequence
-        feedback_.feedback = 900;
-        as_.publishFeedback(feedback_);
-
-        // publish info to the console for the user
-        //ROS_INFO("%s: Executing, creating fibonacci sequence of order %i with seeds %i, %i", action_name_.c_str(), goal->order, feedback_.sequence[0], feedback_.sequence[1]);
-        ROS_INFO("executing action");
-
-        // start executing the action
         if (goal->visualize)
         {
-            // check that preempt has not been requested by the client
-            if (as_.isPreemptRequested() || !ros::ok())
-            {
-                ROS_INFO("%s: Preempted", action_name_.c_str());
-                // set the action state to preempted
-                as_.setPreempted();
-                success = false;
-            }
-            for (int i=0; i<20; i++) {
-                feedback_.feedback = i;
-                as_.publishFeedback(feedback_);
-            }
-            // this sleep is not necessary, the sequence is computed at 1 Hz for demonstration purposes
-            r.sleep();
+            ROS_INFO("Visualization is upcoming");
         }
+
+        // check that preempt has not been requested by the client
+        if (as_.isPreemptRequested() || !ros::ok())
+        {
+            ROS_INFO("%s: Preempted", action_name_.c_str());
+            as_.setPreempted();
+            success = false;
+        }
+
+        ros::ServiceClient triggerClient = nh_.serviceClient<std_srvs::Trigger>("/perception_pipeline/trigger");
+        std_srvs::Trigger srv;
+
+        if (triggerClient.call(srv))
+        {
+            ROS_INFO("Pipeline started");
+            feedback_.feedback = "Pipeline started";
+        }
+        else
+        {
+            ROS_ERROR("Perception-Actionserver failed to connect to Robosherlock-Pipeline.");
+            feedback_.feedback = "Perception-Actionserver failed to connect to Robosherlock-Pipeline.";
+        }
+        as_.publishFeedback(feedback_);
+
+        // this sleep is not necessary, the sequence is computed at 1 Hz for demonstration purposes
+        r.sleep();
+
 
         if(success)
         {
