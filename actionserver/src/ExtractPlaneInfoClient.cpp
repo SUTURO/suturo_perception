15f596d663f9c2027bdf90d73af7843328e47e09
Evan Kapitzke <evan@uni-bremen.de>
2020-01-15 18:30:18 +0100

    fix in Plane client

--- actionserver/src/ExtractPlaneInfoClient.cpp
+++ actionserver/src/ExtractPlaneInfoClient.cpp
@@ -7,13 +7,13 @@
 int main (int argc, char **argv) {
     ros::init (argc, argv, "test_perception");
 
-    actionlib::SimpleActionClient<suturo_perception_msgs::ExtractObjectInfoAction> ac(EPI_NAME, true);
+    actionlib::SimpleActionClient<suturo_perception_msgs::ExtractPlaneInfoAction> ac(EPI_NAME, true);
 
     ROS_INFO("Waiting for action server to start.");
     ac.waitForServer();
 
     ROS_INFO("Action server started, sending goal.");
-    suturo_perception_msgs::ExtractObjectInfoGoal goal;
+    suturo_perception_msgs::ExtractPlaneInfoGoal goal;
     goal.visualize = true;
     ac.sendGoal(goal);
 
