425d7b07286b0b41b5187519f378bb853386d8da
jastock <jastock@uni-bremen.de>
2020-02-21 09:50:55 +0100

    Added Plotting.

--- classificationEvaluation/scripts/rsClassificationEvaluator.py
+++ classificationEvaluation/scripts/rsClassificationEvaluator.py
@@ -6,12 +6,17 @@ import json
 
 import actionlib
 
+import matplotlib.pyplot as plt
+
 import suturo_perception_msgs.msg
 
 
 
 class ResultActionClient:
     def __init__(self):
+        self.k = 5
+        self.results = []
+
         rospy.init_node('rsResultActionClient', anonymous=True)
 
         self.client = actionlib.SimpleActionClient('perception_actionserver', suturo_perception_msgs.msg.ExtractObjectInfoAction)
@@ -29,20 +34,31 @@ class ResultActionClient:
 
         for i in result.detectionData:
             rospy.loginfo("Class: " + i.obj_class + " - confidence: %f", i.confidence_class)
-            self.saveToMd(i.obj_class, i.confidence_class, "KNN - k=13")
+            self.results.append([i.obj_class, i.confidence_class, self.k])
+
+            #self.saveToMd(i.obj_class, i.confidence_class, "KNN - k=13")
 
     def saveToMd(self, objClass, objConfidence, algorithm="KNN - k=9"):
         file = open("evaluation.md", "a")
         file.write("\n| " + objClass + " | " + str(objConfidence) + " | " + algorithm + " |")
         file.close()
 
+    def plotResult(self):
+        for (obj_class, obj_confidence, obj_k) in self.results:
+            plt.scatter(obj_k, obj_confidence)
+
+        plt.show()
 
 
 
 if __name__ == '__main__':
     try:
         resultActionClient = ResultActionClient()
-        result = resultActionClient.sendGoal()
-        resultActionClient.saveResult(result)
+        while 1:
+            resultActionClient.k = input("Please insert k:")
+            result = resultActionClient.sendGoal()
+            resultActionClient.saveResult(result)
+            resultActionClient.plotResult()
+
     except rospy.ROSInterruptException:
         rospy.logerr("The program was interrupted before completion.")
