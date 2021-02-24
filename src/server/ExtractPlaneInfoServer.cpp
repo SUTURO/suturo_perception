1ae38f8401dee0f9da380817fdd8efa9c084c7bb
Evan Kapitzke <evan@uni-bremen.de>
2021-02-24 18:09:54 +0100

    new door message

--- src/server/ExtractPlaneInfoServer.cpp
+++ src/server/ExtractPlaneInfoServer.cpp
@@ -4,15 +4,24 @@ void ExtractPlaneInfoServer::execute(const suturo_perception_msgs::ExtractPlaneI
     std::map<std::string, boost::any> arguments = std::map<std::string, boost::any>();
     arguments["visualize"] = goal->visualize;
     std::vector<std::string> regions = std::vector<std::string>();
-    result.detectionData.clear();
-    pm.run(arguments, result.detectionData);
+    std::vector<ObjectDetectionData> data;
+    pm.run(arguments, data);
 
-    if(!result.detectionData.empty()) {
-        feedback.feedback = "Object Feature detection was successful.";
+    if(!data.empty()) {
+        feedback.feedback = "Door detection was successful.";
+
+        result.detectionData.height = data[0].height;
+        result.detectionData.width = data[0].width;
+        result.detectionData.depth = data[0].depth;
+        result.detectionData.confidence_class = data[0].confidence_class;
+        result.detectionData.name = result.detectionData.obj_class = data[0].name;
+        result.detectionData.pose = data[0].pose;
+        result.detectionData.region = data[0].region;
+
         server.publishFeedback(feedback);
         server.setSucceeded(result);
     } else {
-        feedback.feedback = "No object detection data was perceived. Check standard output for further information.";
+        feedback.feedback = "No door detection data was perceived. Check standard output for further information.";
         server.publishFeedback(feedback);
         server.setAborted();
     }
